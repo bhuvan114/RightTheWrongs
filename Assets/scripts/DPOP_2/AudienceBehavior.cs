d6e8557685c71050a8ca468fc42d50559f996945
bhuvan114 <bhuvan114@users.noreply.github.com>
2016-04-20 18:27:53 -0400

    Presidential Scene

--- Assets/scripts/DPOP_2/AudienceBehavior.cs
+++ Assets/scripts/DPOP_2/AudienceBehavior.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using TreeSharpPlus;
 
 public class AudienceBehavior : MonoBehaviour {
+
+	public Transform position;
+	public Transform look;
+	private GameObject character;
+	private BehaviorAgent behaviorAgent;
+	private Node root = null;
 
 	bool isPlayercontrolled;
 
@@ -12,7 +19,7 @@ public class AudienceBehavior : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+		character = this.gameObject;
 		isPlayercontrolled = false;
 	}
 	
@@ -25,6 +32,33 @@ public class AudienceBehavior : MonoBehaviour {
 		if (!isPlayercontrolled) {
 
 			//Default Audience Behavior
+			if (root == null) {
+				root = default_go_look ();
+				behaviorAgent = new BehaviorAgent (root);
+				BehaviorManager.Instance.Register (behaviorAgent);
+				behaviorAgent.StartBehavior ();
+			} else if (!root.IsRunning) {
+				root = null;
+			}
 		}
+	}
+
+	protected Node default_go()
+	{
+		Val<Vector3> target = Val.V (() => position.position);
+		return new Sequence (character.GetComponent<BehaviorMecanim> ().Node_GoTo (target), new LeafWait (1000));
+	}
+
+	protected Node default_look()
+	{
+		Val<Vector3> target = Val.V (() => look.position);
+		return new Sequence (//character.GetComponent<BehaviorMecanim>().ST_TurnToFace(target),
+							 character.GetComponent<BehaviorMecanim>().Node_OrientTowards(target),
+								new LeafWait (1000));
+	}
+
+	protected Node default_go_look()
+	{
+		return new Sequence (this.default_go(), this.default_go());
 	}
 }
---
+++ Assets/scripts/DPOP_2/BC_Default_Behavior.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BC_Default_Behavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

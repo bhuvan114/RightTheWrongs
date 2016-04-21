5877ea4af49a33cf3fc36860264c75413bea3c9d
bhuvan114 <bhuvan114@users.noreply.github.com>
2016-04-21 18:54:06 -0400

    Added to Scene and hit/meet affordances.

---
+++ Assets/scripts/Affordances/Hit.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using TreeSharpPlus;
+
+using POPL.Planner;
+
+public class Hit : Affordance {
+
+	public Hit(SmartCharacter afdnt, SmartObject afdee){
+		
+		affodant = afdnt;
+		affordee = afdee;
+		initialize ();
+	}
+
+	void initialize() {
+		
+		base.initialize ();
+
+		preconditions.Add (new Condition(affordantName, affordeeName, "InFront", true));
+		effects.Add(new Condition(affordeeName, affordantName, "HitBy", true));
+		
+
+	}
+
+	//Behaviour Tree here
+	public Node execute()
+	{
+		Debug.Log("Hit execute");
+		return new Sequence(this.HitAnimation());
+	}
+
+	Node HitAnimation()
+	{
+		Debug.Log("Hit animation");
+		return new Sequence(
+			affodant.GetComponent<BehaviorMecanim>().Node_HandAnimation("BASH", true), 
+			affordee.GetComponent<BehaviorMecanim>().Node_BodyAnimation("DYING", true),
+			new LeafWait(500)
+		);
+	}
+}
--- Assets/scripts/Affordances/Meet.cs
+++ Assets/scripts/Affordances/Meet.cs
@@ -23,5 +23,18 @@ public class Meet : Affordance {
 	
 	//Behaviour Tree here
 	public void execute() {
+		Debug.Log("Meet execute");
+		return new Sequence(this.HitAnimation());
+	}
+
+	Node MeetAnimation()
+	{
+		Debug.Log("MeetAnimation");
+		Val<Vector3> target = Val.V (() => affordee.gameObject.transform.position);
+		Val<Vector3> target2 = Val.V (() => affodant.gameObject.transform.position);
+		return new Sequence (affodant.GetComponent<BehaviorMecanim> ().Node_GoToUpToRadius (target, 1), 
+			affodant.GetComponent<BehaviorMecanim>().ST_TurnToFace(target),
+			affordee.GetComponent<BehaviorMecanim>().ST_TurnToFace(target1),
+			new LeafWait (1000));
 	}
 }
